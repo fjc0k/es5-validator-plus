4de4f9e91f11c6b284579f36b8a8071f9978dc49
方剑成 <fjc0kb@gmail.com>
2022-01-29 10:01:37 +0800

    feat: support wildcards

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 #!/usr/bin/env node
 
-const { extname } = require('path');
-const { readFileSync } = require('fs')
+const { extname } = require("path");
+const { readFileSync } = require("fs");
 
-const acorn = require('acorn');
-const chalk = require('chalk');
-const { codeFrameColumns } = require('@babel/code-frame');
-const beautify = require('js-beautify').js;
-const { isMinified } = require('is-minified-performant');
+const acorn = require("acorn");
+const chalk = require("chalk");
+const { codeFrameColumns } = require("@babel/code-frame");
+const beautify = require("js-beautify").js;
+const { isMinified } = require("is-minified-performant");
+const globby = require("globby");
 
-const { fetch } = require('./fetch');
+const { fetch } = require("./fetch");
 
-const debug = require('debug')('es5-validator');
+const debug = require("debug")("es5-validator");
 // const debug = console.log;
 
 sourceFiles = process.argv.slice(2);
 const silent = process.env.SILENT === "true";
 
 async function main() {
+  sourceFiles = (
+    await Promise.all(
+      sourceFiles.map(async (file) => {
+        return isRemoteFile(file)
+          ? [file]
+          : await globby(file, {
+              onlyFiles: true,
+            });
+      })
+    )
+  ).flat();
   printResult(await validate(sourceFiles));
 }
 
@@ -32,99 +44,128 @@ async function validate(filePaths) {
 
   debug(`got files:`, filePaths);
 
-  const jsFilePaths = filePaths.filter(path => {
-    if (extname(path) === '.js') {
+  const jsFilePaths = filePaths.filter((path) => {
+    if (extname(path) === ".js") {
       return true;
     }
 
-    debug(path, 'not a js file, ignored');
+    debug(path, "not a js file, ignored");
   });
 
   if (!jsFilePaths || !jsFilePaths.length) {
-    console.log('[es5-validator] No file to validate: $ es5-validator FILE_1_TO_VALIDATE.js FILE_2_TO_VALIDATE.js');
+    console.log(
+      "[es5-validator] No file to validate: $ es5-validator FILE_1_TO_VALIDATE.js FILE_2_TO_VALIDATE.js"
+    );
 
     process.exit(0);
   }
 
-  await Promise.all(jsFilePaths.map(async fileName => {
-    debug(`validate es5 for ${fileName}`);
+  await Promise.all(
+    jsFilePaths.map(async (fileName) => {
+      debug(`validate es5 for ${fileName}`);
 
-    const rawSource = await fetchFileContent(fileName).catch(error => {
-      console.error('fetchFileContent', error);
+      const rawSource = await fetchFileContent(fileName).catch((error) => {
+        console.error("fetchFileContent", error);
 
-      process.exit(1);
-    });
-
-    debug(`rawSource:`, rawSource);
-
-    compressed = isCompressed(rawSource);
-
-    const source = compressed ?
-      beautify(rawSource, { indent_size: 2, space_in_empty_paren: true }) :
-      rawSource;
-
-    compressed && debug(`source after beautified:`, source);
-
-    try {
-      acorn.parse(source, {
-        ecmaVersion: 5,
+        process.exit(1);
       });
 
-      debug(`parse success ${fileName}`);
-    } catch (e) {
-      debug(`parse failed ${fileName}, error:`, e);
+      debug(`rawSource:`, rawSource);
 
-      errors = [
-        chalk.black.bgRed('Error') +
-        chalk.red(
-          ': ECMAScript 5 validate failed when parsing',
-          `${chalk.green.bold(fileName +
-            (compressed || isRemoteFile(fileName) ? ` (formatted) (${e.loc.line}, ${e.loc.column})` : `:${e.loc.line}:${e.loc.column}`)
-          )}`
-        ),
-      ];
+      compressed = isCompressed(rawSource);
 
-      errors.push('');
-      errors.push('');
+      const source = compressed
+        ? beautify(rawSource, { indent_size: 2, space_in_empty_paren: true })
+        : rawSource;
 
-      errors.push(codeFrameColumns(source, {
-        start: e.loc,
-      }, {
-        highlightCode: true,
-        forceColor: true,
-        message: 'Invalid ECMAScript 5 syntax',
-        linesAbove: 10,
-        linesBelow: 2,
-      }));
-    }
-  }));
+      compressed && debug(`source after beautified:`, source);
+
+      try {
+        acorn.parse(source, {
+          ecmaVersion: 5,
+        });
+
+        debug(`parse success ${fileName}`);
+      } catch (e) {
+        debug(`parse failed ${fileName}, error:`, e);
+
+        errors = [
+          chalk.black.bgRed("Error") +
+            chalk.red(
+              ": ECMAScript 5 validate failed when parsing",
+              `${chalk.green.bold(
+                fileName +
+                  (compressed || isRemoteFile(fileName)
+                    ? ` (formatted) (${e.loc.line}, ${e.loc.column})`
+                    : `:${e.loc.line}:${e.loc.column}`)
+              )}`
+            ),
+        ];
+
+        errors.push("");
+        errors.push("");
+
+        errors.push(
+          codeFrameColumns(
+            source,
+            {
+              start: e.loc,
+            },
+            {
+              highlightCode: true,
+              forceColor: true,
+              message: "Invalid ECMAScript 5 syntax",
+              linesAbove: 10,
+              linesBelow: 2,
+            }
+          )
+        );
+      }
+    })
+  );
 
   return { errors, compressed };
 }
 
 function printResult({ errors = [], compressed = false } = {}) {
-  console.log('');
+  console.log("");
 
   const compatible = errors.length === 0;
 
   if (compatible) {
-    console.log(chalk.greenBright('[es5-validator] Congratulations! Your code is ES5 Compatible. It\'s ready to ship to production.'));
+    console.log(
+      chalk.greenBright(
+        "[es5-validator] Congratulations! Your code is ES5 Compatible. It's ready to ship to production."
+      )
+    );
   } else {
     if (compressed) {
-      console.log(chalk.italic(`[es5-validator] NOTICE: It's hard to locate the problem when code is compressed, so it will be formatted before validation.`));
-      console.log('');
+      console.log(
+        chalk.italic(
+          `[es5-validator] NOTICE: It's hard to locate the problem when code is compressed, so it will be formatted before validation.`
+        )
+      );
+      console.log("");
     }
 
-    console.log(chalk.redBright('[es5-validator] Your code is not ES5 Compatible. It\'s not ready to ship to production, otherwise it will break you App on iOS 9 or iOS 10.'));
-    console.log('');
-    console.error(errors.join('\n'));
+    console.log(
+      chalk.redBright(
+        "[es5-validator] Your code is not ES5 Compatible. It's not ready to ship to production, otherwise it will break you App on iOS 9 or iOS 10."
+      )
+    );
+    console.log("");
+    console.error(errors.join("\n"));
   }
 
-  console.log('');
-  console.log(chalk.cyan(`Give a star ❤️  if it helped you https://github.com/legend80s/es5-validator.`));
-  console.log('');
+  console.log("");
+  console.log(
+    chalk.cyan(
+      `Give a star ❤️  if it helped you https://github.com/legend80s/es5-validator.`
+    )
+  );
+  console.log("");
 
-  process.exit((compatible || silent) ? 0 : 1)
+  process.exit(compatible || silent ? 0 : 1);
 }
 
 /**
@@ -160,5 +201,5 @@ async function fetchFileContent(filePath) {
  * @returns {boolean}
  */
 function isCompressed(source) {
-  return isMinified(source)
+  return isMinified(source);
 }
